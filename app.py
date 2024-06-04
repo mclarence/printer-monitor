7b5cc0574c52290de08dada16c102234e9760c74
Clarence <c.mes@cm0.dev>
2024-06-05 09:48:34 +1000

    update template

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 printer_oids = configparser.ConfigParser()
+printers = configparser.ConfigParser()
+
+printers.read('printers.ini')
 printer_oids.read('printer_oids.ini')
 
 snmp_oid = {}
+printers_dict = {}
 
 for section in printer_oids.sections():
     if section not in snmp_oid:
@@ -33,21 +37,12 @@ for section in printer_oids.sections():
         else:
             print("Unrecognized key: " + key)
 
-printers = configparser.ConfigParser()
-printers.read('printers.ini')
-
-printers_dict = {}
-
 for section in printers.sections():
     if section not in printers_dict:
         printers_dict[section] = {}
 
     for key, value in printers.items(section):
         printers_dict[section][key] = value
-        print(key, value)
-
-print(printers_dict)
-print(snmp_oid)
 
 async def get_toner(ip, model, toner):
    client = PyWrapper(Client(ip, V2C("public")))
@@ -59,27 +54,60 @@ async def get_drum(ip, model, colour):
    output = await client.get(snmp_oid[model]["drum"][colour])
    return output
 
-@app.route('/')
+async def get_printer_status(printer):
+    hostname = printers[printer]['hostname']
+    model = printers[printer]['model']
+    toner_coro = [
+        get_toner(hostname, model, color) for color in ['c', 'm', 'y', 'k']
+    ]
+
+    drum_coro = [
+        get_drum(hostname, model, color) for color in ['c', 'm', 'y', 'k']
+    ]
+
+    toner_results = await asyncio.gather(*toner_coro)
+    drum_results = await asyncio.gather(*drum_coro)
+
+    return {
+        "friendly_name": printer,
+        "hostname": hostname,
+        "toner": dict(zip(['c', 'm', 'y', 'k'], toner_results)),
+        "drum": dict(zip(['c', 'm', 'y', 'k'], drum_results))
+    }
+
+def get_set_event_loop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as e:
+        if e.args[0].startswith('There is no current event loop'):
+            asyncio.set_event_loop(asyncio.new_event_loop())
+            return asyncio.get_event_loop()
+        raise e
+
+@app.route('/', methods=['GET'])
 def index():
-    printer_statuses = {}
+    tasks = [get_printer_status(printer) for printer in printers_dict.keys()]
 
-    for section in printers.sections():
-        printer_status = {
-            "hostname": printers[section]['hostname'],
-            "toner": {
-                "c": asyncio.run(get_toner(printers[section]['hostname'], printers[section]['model'], "c")),
-                "m": asyncio.run(get_toner(printers[section]['hostname'], printers[section]['model'], "m")),
-                "y": asyncio.run(get_toner(printers[section]['hostname'], printers[section]['model'], "y")),
-                "k": asyncio.run(get_toner(printers[section]['hostname'], printers[section]['model'], "k"))
-            },
-            "drum": {
-                "c": asyncio.run(get_drum(printers[section]['hostname'], printers[section]['model'], "c")),
-                "m": asyncio.run(get_drum(printers[section]['hostname'], printers[section]['model'], "m")),
-                "y": asyncio.run(get_drum(printers[section]['hostname'], printers[section]['model'], "y")),
-                "k": asyncio.run(get_drum(printers[section]['hostname'], printers[section]['model'], "k"))
-            }
-        }
+    printer_statuses = get_set_event_loop().run_until_complete(asyncio.gather(*tasks))
+    low_consumables = []
 
-        printer_statuses[section] = printer_status
+    for printer in printer_statuses:
+        for toner in printer['toner']:
+            if printer['toner'][toner] < 10:
+                low_consumables.append({
+                    "printer": printer['friendly_name'],
+                    "type": "toner",
+                    "color": toner,
+                    "level": printer['toner'][toner]
+                })
 
-    return render_template('index.html', printers=printer_statuses)
+        for drum in printer['drum']:
+            if printer['drum'][drum] < 10:
+                low_consumables.append({
+                    "printer": printer['friendly_name'],
+                    "type": "drum",
+                    "color": drum,
+                    "level": printer['drum'][drum]
+                })
+
+    return render_template('index.html', printers=printer_statuses, low_consumables=low_consumables)
